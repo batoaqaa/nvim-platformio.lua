a65d38035672382a50892365654ad479bca6c2df
batoaqaa <ahmed.batoaq@gmail.com>
2024-09-02 14:49:30 +0300

    update monitor winbar and preview wrap)

--- lua/platformio/pioinit.lua
+++ lua/platformio/pioinit.lua
@@ -11,124 +11,131 @@ local utils = require("platformio.utils")
 local previewers = require("telescope.previewers")
 
 local boardentry_maker = function(opts)
-  local displayer = entry_display.create({
-    separator = "▏",
-    items = {
-      { width = 35 },
-      { width = 20 },
-      { width = 15 },
-    },
-  })
+	local displayer = entry_display.create({
+		separator = "▏",
+		items = {
+			{ width = 35 },
+			{ width = 20 },
+			{ width = 15 },
+		},
+	})
 
-  local make_display = function(entry)
-    return displayer({
-      entry.value.name,
-      entry.value.vendor,
-      entry.value.platform,
-    })
-  end
+	local make_display = function(entry)
+		return displayer({
+			entry.value.name,
+			entry.value.vendor,
+			entry.value.platform,
+		})
+	end
 
-  return function(entry)
-    return make_entry.set_default_entry_mt({
-      value = {
-        id = entry.id,
-        name = entry.name,
-        vendor = entry.vendor,
-        platform = entry.platform,
-        data = entry,
-      },
-      ordinal = entry.name .. " " .. entry.vendor .. " " .. entry.platform,
-      display = make_display,
-    }, opts)
-  end
+	return function(entry)
+		return make_entry.set_default_entry_mt({
+			value = {
+				id = entry.id,
+				name = entry.name,
+				vendor = entry.vendor,
+				platform = entry.platform,
+				data = entry,
+			},
+			ordinal = entry.name .. " " .. entry.vendor .. " " .. entry.platform,
+			display = make_display,
+		}, opts)
+	end
 end
 
 local function pick_framework(board_details)
-  local opts = {}
-  pickers
-      .new(opts, {
-        prompt_title = "frameworks",
-        finder = finders.new_table({
-          results = board_details["frameworks"],
-        }),
-        attach_mappings = function(prompt_bufnr, _)
-          actions.select_default:replace(function()
-            actions.close(prompt_bufnr)
-            local selection = action_state.get_selected_entry()
-            local selected_framework = selection[1]
-            local command = "pio project init --ide=vim --board "
-                .. board_details["id"]
-                .. ' --project-option "framework='
-                .. selected_framework
-                .. '"'
-                .. utils.extra
-            utils.ToggleTerminal(command, "float")
-          end)
-          return true
-        end,
-        sorter = conf.generic_sorter(opts),
-      })
-      :find()
+	local opts = {}
+	pickers
+		.new(opts, {
+			prompt_title = "frameworks",
+			finder = finders.new_table({
+				results = board_details["frameworks"],
+			}),
+			attach_mappings = function(prompt_bufnr, _)
+				actions.select_default:replace(function()
+					actions.close(prompt_bufnr)
+					local selection = action_state.get_selected_entry()
+					local selected_framework = selection[1]
+					local command = "pio project init --ide=vim --board "
+						.. board_details["id"]
+						.. ' --project-option "framework='
+						.. selected_framework
+						.. '"'
+						.. utils.extra
+					utils.ToggleTerminal(command, "float")
+				end)
+				return true
+			end,
+			sorter = conf.generic_sorter(opts),
+		})
+		:find()
 end
 
 local function pick_board(json_data)
-  local opts = {}
-  pickers
-      .new(opts, {
-        prompt_title = "Boards",
-        finder = finders.new_table({
-          results = json_data,
-          entry_maker = opts.entry_maker or boardentry_maker(opts),
-        }),
-        attach_mappings = function(prompt_bufnr, _)
-          actions.select_default:replace(function()
-            actions.close(prompt_bufnr)
-            local selection = action_state.get_selected_entry()
-            pick_framework(selection["value"]["data"])
-          end)
-          return true
-        end,
-        previewer = previewers.new_buffer_previewer({
-          title = "Board Info",
-          define_preview = function(self, entry, _)
-            local json = utils.strsplit(vim.inspect(entry["value"]["data"]), "\n")
-            vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, json)
-          end,
-        }),
-        sorter = conf.generic_sorter(opts),
-      })
-      :find()
+	local opts = {}
+	pickers
+		.new(opts, {
+			prompt_title = "Boards",
+			finder = finders.new_table({
+				results = json_data,
+				entry_maker = opts.entry_maker or boardentry_maker(opts),
+			}),
+			attach_mappings = function(prompt_bufnr, _)
+				actions.select_default:replace(function()
+					actions.close(prompt_bufnr)
+					local selection = action_state.get_selected_entry()
+					pick_framework(selection["value"]["data"])
+				end)
+				return true
+			end,
+			previewer = previewers.new_buffer_previewer({
+				title = "Board Info",
+				define_preview = function(self, entry, _)
+					local json = utils.strsplit(vim.inspect(entry["value"]["data"]), "\n")
+					local win = self.state.winid
+					local bufnr = self.state.bufnr
+					vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, json)
+					vim.defer_fn(function()
+						vim.api.nvim_set_option_value("wrap", true, { scope = "local", win = win })
+						vim.api.nvim_set_option_value("linebreak", true, { scope = "local", win = win })
+						vim.api.nvim_set_option_value("wrapmargin", 2, { buf = bufnr })
+					end, 0)
+				end,
+			}),
+			sorter = conf.generic_sorter(opts),
+		})
+		:find()
 end
 
 function M.pioinit()
-  if not utils.pio_install_check() then
-    return
-  end
+	if not utils.pio_install_check() then
+		return
+	end
 
-  -- Read stdout
-  local command = "pio boards --json-output"
-  local handel = io.popen(command .. utils.devNul)
-  if not handel then
-    return
-  end
-  local json_str = handel:read("*a")
-  handel:close()
+	-- Read stdout
+	local command = "pio boards --json-output"
+	local handel = io.popen(command .. utils.devNul)
+	if not handel then
+		return
+	end
+	local json_str = handel:read("*a")
+	handel:close()
 
-  if #json_str == 0 then
-    -- read stderr
-    handel = io.popen(command .. " 2>&1")
-    if not handel then
-      return
-    end
-    local command_output = handel:read("*a")
-    handel:close()
-    vim.notify("Some error occured while executing `" .. command .. "`', command output: \n", vim.log.levels.WARN)
-    print(command_output)
-    return
-  end
+	if #json_str == 0 then
+		-- read stderr
+		handel = io.popen(command .. " 2>&1")
+		if not handel then
+			return
+		end
+		local command_output = handel:read("*a")
+		handel:close()
+		vim.notify("Some error occured while executing `" .. command .. "`', command output: \n", vim.log.levels.WARN)
+		print(command_output)
+		return
+	end
 
-  local json_data = vim.json.decode(json_str)
-  pick_board(json_data)
+	local json_data = vim.json.decode(json_str)
+	pick_board(json_data)
 end
 
 return M
--- lua/platformio/piolib.lua
+++ lua/platformio/piolib.lua
@@ -13,109 +13,116 @@ local utils = require("platformio.utils")
 local previewers = require("telescope.previewers")
 
 local libentry_maker = function(opts)
-  local displayer = entry_display.create({
-    separator = "▏",
-    items = {
-      { width = 20 },
-      { width = 20 },
-      { remaining = true },
-    },
-  })
+	local displayer = entry_display.create({
+		separator = "▏",
+		items = {
+			{ width = 20 },
+			{ width = 20 },
+			{ remaining = true },
+		},
+	})
 
-  local make_display = function(entry)
-    return displayer({
-      entry.value.name,
-      entry.value.owner,
-      entry.value.description,
-    })
-  end
+	local make_display = function(entry)
+		return displayer({
+			entry.value.name,
+			entry.value.owner,
+			entry.value.description,
+		})
+	end
 
-  return function(entry)
-    return make_entry.set_default_entry_mt({
-      value = {
-        name = entry.name,
-        owner = entry.owner.username,
-        description = entry.description,
-        data = entry,
-      },
-      ordinal = entry.name .. " " .. entry.owner.username .. " " .. entry.description,
-      display = make_display,
-    }, opts)
-  end
+	return function(entry)
+		return make_entry.set_default_entry_mt({
+			value = {
+				name = entry.name,
+				owner = entry.owner.username,
+				description = entry.description,
+				data = entry,
+			},
+			ordinal = entry.name .. " " .. entry.owner.username .. " " .. entry.description,
+			display = make_display,
+		}, opts)
+	end
 end
 
 local function pick_library(json_data)
-  local opts = {}
-  table.sort(json_data["items"], function(lhs, rhs)
-    return lhs.popularity_rank > rhs.popularity_rank
-  end)
-  pickers
-      .new(opts, {
-        prompt_title = "Libraries",
-        finder = finders.new_table({
-          results = json_data["items"],
-          entry_maker = opts.entry_maker or libentry_maker(opts),
-        }),
-        attach_mappings = function(prompt_bufnr, _)
-          actions.select_default:replace(function()
-            actions.close(prompt_bufnr)
-            local selection = action_state.get_selected_entry()
-            local pkg_name = selection["value"]["owner"] .. "/" .. selection["value"]["name"]
-            local command = 'pio pkg install --library "' .. pkg_name .. '"' .. utils.extra
-            utils.ToggleTerminal(command, "float")
-          end)
-          return true
-        end,
+	local opts = {}
+	table.sort(json_data["items"], function(lhs, rhs)
+		return lhs.popularity_rank > rhs.popularity_rank
+	end)
+	pickers
+		.new(opts, {
+			prompt_title = "Libraries",
+			finder = finders.new_table({
+				results = json_data["items"],
+				entry_maker = opts.entry_maker or libentry_maker(opts),
+			}),
+			attach_mappings = function(prompt_bufnr, _)
+				actions.select_default:replace(function()
+					actions.close(prompt_bufnr)
+					local selection = action_state.get_selected_entry()
+					local pkg_name = selection["value"]["owner"] .. "/" .. selection["value"]["name"]
+					local command = 'pio pkg install --library "' .. pkg_name .. '"' .. utils.extra
+					utils.ToggleTerminal(command, "float")
+				end)
+				return true
+			end,
 
-        previewer = previewers.new_buffer_previewer({
-          title = "Package Info",
-          define_preview = function(self, entry, _)
-            local json = utils.strsplit(vim.inspect(entry["value"]["data"]), "\n")
-            vim.api.nvim_buf_set_lines(self.state.bufnr, 0, -1, false, json)
-          end,
-        }),
-        sorter = conf.generic_sorter(opts),
-      })
-      :find()
+			previewer = previewers.new_buffer_previewer({
+				title = "Package Info",
+				define_preview = function(self, entry, _)
+					local json = utils.strsplit(vim.inspect(entry["value"]["data"]), "\n")
+					local bufnr = self.state.bufnr
+					local win = self.state.winid
+					vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, json)
+					vim.defer_fn(function()
+						vim.api.nvim_set_option_value("wrap", true, { scope = "local", win = win })
+						vim.api.nvim_set_option_value("linebreak", true, { scope = "local", win = win })
+						vim.api.nvim_set_option_value("wrapmargin", 2, { buf = bufnr })
+					end, 0)
+				end,
+			}),
+			sorter = conf.generic_sorter(opts),
+		})
+		:find()
 end
 
 function M.piolib(lib_arg_list)
-  if not utils.pio_install_check() then
-    return
-  end
+	if not utils.pio_install_check() then
+		return
+	end
 
-  local lib_str = ""
+	local lib_str = ""
 
-  for _, v in pairs(lib_arg_list) do
-    lib_str = lib_str .. v .. "+"
-  end
+	for _, v in pairs(lib_arg_list) do
+		lib_str = lib_str .. v .. "+"
+	end
 
-  local url = "https://api.registry.platformio.org/v3/search"
-  local res = curl.get(url, {
-    insecure = true,
-    timeout = 20000,
-    headers = { content_type = "application/json" },
-    query = {
-      query = lib_str,
-      -- page = 1,
-      -- limit = 1,
-    },
-  })
+	local url = "https://api.registry.platformio.org/v3/search"
+	local res = curl.get(url, {
+		insecure = true,
+		timeout = 20000,
+		headers = { content_type = "application/json" },
+		query = {
+			query = lib_str,
+			-- page = 1,
+			-- limit = 1,
+		},
+	})
 
-  if res["status"] == 200 then
-    local json_data = vim.json.decode(res["body"])
+	if res["status"] == 200 then
+		local json_data = vim.json.decode(res["body"])
 
-    pick_library(json_data)
-  else
-    vim.notify(
-      "API Request to platformio return HTTP code: "
-      .. res["status"]
-      .. "\nplease run `curl -LI "
-      .. url
-      .. "` for complete information",
-      vim.log.levels.ERROR
-    )
-  end
+		pick_library(json_data)
+	else
+		vim.notify(
+			"API Request to platformio return HTTP code: "
+				.. res["status"]
+				.. "\nplease run `curl -LI "
+				.. url
+				.. "` for complete information",
+			vim.log.levels.ERROR
+		)
+	end
 end
 
 return M
