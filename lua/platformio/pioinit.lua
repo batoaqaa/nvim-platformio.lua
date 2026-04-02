2d911bf8592173df553ae4d9e785776cf764af90
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-02 11:57:34 +0300

    update

--- lua/platformio/pioinit.lua
+++ lua/platformio/pioinit.lua
@@ -61,7 +61,7 @@ local function pick_framework(board_details)
           local command = 'pio project init --board ' .. board_details['id'] .. ' --project-option "framework=' .. selected_framework .. '"'
           -- .. utils.extra
           utils.ToggleTerminal(command, 'float', function()
-            boilerplate_gen(selected_framework, vim.g.platformioRootDir)
+            boilerplate_gen(selected_framework, vim.g.platformioRootDir .. '/src')
             vim.cmd(':PioLSP')
           end)
         end)
