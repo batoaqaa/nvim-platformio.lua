ef61a2920c790d108df8bdddb65ce97a42c39b9c
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-02 22:54:33 +0300

    update

--- minimal_config.lua
+++ minimal_config.lua
@@ -190,6 +190,18 @@ local plugins = {
   --   opts_extend = { 'sources.default' },
   -- },
   --
+  --
+
+  {
+    'akinsho/bufferline.nvim',
+    config = function()
+      require('bufferline').setup({
+        options = {
+          mode = 'tabs',
+        },
+      })
+    end,
+  },
   {
     'famiu/bufdelete.nvim',
     keys = {
@@ -201,13 +213,6 @@ local plugins = {
         desc = 'Delete buffer',
       },
     },
-  },
-  {
-    'akinsho/bufferline.nvim',
-    version = '*',
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    config = true,
-    -- config = true is shorthand for config = function() require('bufferline').setup() end
   },
   {
     'nvim-tree/nvim-tree.lua',
