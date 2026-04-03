c0a33b0f2bd0f594497b81c2c635ba5a590741ed
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-03 06:42:58 +0300

    update

--- minimal_config.lua
+++ minimal_config.lua
@@ -96,12 +96,12 @@ keymap('n', '<leader>bb', ':bprevious<CR>', { desc = '[B]efore Buffer' })
 keymap('n', '<leader>ba', ':bnext<CR>', { desc = '[A]fter Buffer' })
 keymap('n', '<leader>bs', ':ball<CR>', { desc = '[S]how AllOpened Buffers' })
 keymap('n', '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', { desc = 'Toggle Pin' })
+
 -- keymap('n', '<leader>bd', '<Cmd>bdelete<CR>', { desc = '[D]elete Buffer' })
 -- keymap('n', '<leader>bd', ':bp | bd #<CR>', { desc = '[D]elete Buffer' })
 keymap('n', '<leader>bd', function()
   -- local bufnr = vim.api.nvim_get_current_buf()
   local bufs = vim.fn.getbufinfo({ buflisted = 1 })
-
   if #bufs <= 1 then
     -- If it's the last buffer, create a new empty one first
     -- This prevents focus from jumping to NvimTree
