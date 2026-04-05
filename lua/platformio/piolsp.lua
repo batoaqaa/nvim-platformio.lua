dde0a812395509c6632663c1a6b192bba487ec81
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-05 07:32:39 +0300

    update

--- lua/platformio/piolsp.lua
+++ lua/platformio/piolsp.lua
@@ -24,6 +24,21 @@ local function gitignore_lsp_configs(config_file)
     file:close()
   end
 end
+function M.lsp_restart(name)
+  if vim.fn.has('nvim-0.12') then
+    local clients = vim.lsp.get_clients({ name = name })
+
+    if #clients > 0 then
+      -- Client is active, try to restart
+      local ok, err = pcall(vim.cmd.lsp, { args = { 'restart', 'clangd' } })
+      if not ok then
+        vim.notify('LSP ' .. name .. ' restart failed: ' .. err)
+      end
+    end
+  else
+    vim.cmd('LspRestart')
+  end
+end
 
 function M.piolsp()
   if not utils.pio_install_check() then
@@ -34,17 +49,18 @@ function M.piolsp()
   utils.shell_cmd_blocking('pio run -t compiledb')
   gitignore_lsp_configs('compile_commands.json')
 
-  if vim.fn.has('nvim-0.12') then
-    -- local clangd = vim.lsp.get_clients({ name = 'clangd' })[1]
-    -- if clangd then
-    --   -- print('number of attaced: ' .. #clangd.attached_buffers)
-    --   -- print('piolsp: lsp restart ' .. clangd.name)
-    pcall(vim.cmd.lsp, { args = { 'restart', 'clangd' } })
-    -- vim.cmd('lsp restart clangd')
-    -- end
-  else
-    vim.cmd('LspRestart')
-  end
+  -- if vim.fn.has('nvim-0.12') then
+  -- local clangd = vim.lsp.get_clients({ name = 'clangd' })[1]
+  -- if clangd then
+  --   -- print('number of attaced: ' .. #clangd.attached_buffers)
+  --   -- print('piolsp: lsp restart ' .. clangd.name)
+  -- pcall(vim.cmd.lsp, { args = { 'restart', 'clangd' } })
+  M.lsp_restart('clangd')
+  -- vim.cmd('lsp restart clangd')
+  -- end
+  -- else
+  -- vim.cmd('LspRestart')
+  -- end
 
   vim.notify('LSP: compile_commands.jsoncon generation/update completed!', vim.log.levels.INFO)
 end
