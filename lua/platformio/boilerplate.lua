16456e8afa75983b5639ff17a7188af10ec813ef
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-06 16:55:21 +0300

    update

--- lua/platformio/boilerplate.lua
+++ lua/platformio/boilerplate.lua
@@ -38,7 +38,7 @@ monitor_rts = 1	  ; 1 combination to reset esp32c6 (Table 32.3-2. CDC-ACM Settin
 monitor_dtr = 0   ; 0 // pio dev mon --rts=0 --dtr=0 then pio dev mon --rts=1 dtr=0
 
 extra_scripts = pre:extra_script.py
-extra_scripts = post:generate_compile_commands.py
+                post:generate_compile_commands.py
 
 lib_ldf_mode = deep   ;Library dependencies Finder ldf
 ]],
