467368767700c54e66b1d607184d91a5153be170
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-06 14:26:36 +0300

    update

--- lua/platformio/boilerplate.lua
+++ lua/platformio/boilerplate.lua
@@ -21,7 +21,8 @@ void loop() {
 boilerplate['platformio.ini'] = {
   content = [[
 [platformio]
-core_dir = ]] .. vim.env.PLATFORMIO_CORE_DIR .. [[default_envs = 
+core_dir = $vim.env.PLATFORMIO_CORE_DIR
+default_envs = 
 ;default_envs = uno, nodemcu
 platforms_dir = ${platformio.core_dir}/platforms
 packages_dir = ${platformio.core_dir}/packages
