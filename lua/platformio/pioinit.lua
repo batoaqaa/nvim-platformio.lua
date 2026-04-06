dcf850428f87a9de826fd101cc01300f5fcf7ac4
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-06 16:47:19 +0300

    update

--- lua/platformio/pioinit.lua
+++ lua/platformio/pioinit.lua
@@ -62,12 +62,8 @@ local function pick_framework(board_details)
           -- pio project init --board <BOARD_ID> \
           --   -O "packages_dir = ${platformio.core_dir}/esp32c3/packages" \
           --   -O "platforms_dir = ${platformio.core_dir}/esp32c3/platforms"
-          local command = [[pio project init --board ]]
-            .. board_details['id']
-            .. [[ -O "lib_ldf_mode = deep" -O "extra_scripts = pre:extra_script.py" -O "framework=]]
-            .. selected_framework
-            .. [["]]
-          command = command .. [[ && pio run -t compiledb]]
+          local command = 'pio project init --board ' .. board_details['id'] .. ' -O "framework=' .. selected_framework .. '"'
+          command = command .. ' && pio run -t compiledb'
 
           utils.ToggleTerminal(command, 'float')
           vim.defer_fn(function()
