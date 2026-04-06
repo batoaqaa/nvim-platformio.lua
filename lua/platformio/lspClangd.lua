98b5e95990eb9115e37ac58e2b412ec06c7a5082
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-06 06:15:40 +0300

    update

--- lua/platformio/lspClangd.lua
+++ lua/platformio/lspClangd.lua
@@ -70,7 +70,7 @@ local mok, mason_lspconfig = pcall(require, 'mason-lspconfig')
 if mok then
   mason_lspconfig.setup({
     -- ensure_installed = { 'clangd', 'pyrefly' },
-    ensure_installed = { 'clangd', 'lua_ls', 'pyrefly' },
+    ensure_installed = { 'clangd', 'lua_ls', 'pyrefly', 'yamlls' },
     automatic_enable = true, -- this will automatically enable LSP servers after lsp.config
   })
 end
@@ -184,6 +184,46 @@ local lua_ls = {
   },
 }
 vim.lsp.config('lua_ls', lua_ls)
+
+local yamlls = {
+  -- on_attach = opts.on_attach,
+  cmd = { 'yaml-language-server', '--stdio' },
+  filetypes = { 'yaml', 'yaml.docker-compose', 'yaml.gitlab' },
+  settings = {
+    yaml = {
+      hover = true,
+      validate = false,
+      completion = true,
+      keyOrdering = false,
+      format = { enabled = false },
+      redhat = {
+        telemetry = { enabled = false },
+      },
+      schemaStore = {
+        enable = true,
+        url = 'https://www.schemastore.org/api/json/catalog.json',
+      },
+      schemas = {
+        kubernetes = '*.yaml',
+        ['http://json.schemastore.org/github-workflow'] = '.github/workflows/*',
+        ['http://json.schemastore.org/github-action'] = '.github/action.{yml,yaml}',
+        ['https://raw.githubusercontent.com/microsoft/azure-pipelines-vscode/master/service-schema.json'] = 'azure-pipelines.yml',
+        ['http://json.schemastore.org/ansible-stable-2.9'] = 'roles/tasks/*.{yml,yaml}',
+        ['http://json.schemastore.org/prettierrc'] = '.prettierrc.{yml,yaml}',
+        ['http://json.schemastore.org/kustomization'] = 'kustomization.{yml,yaml}',
+        ['http://json.schemastore.org/ansible-playbook'] = '*play*.{yml,yaml}',
+        ['http://json.schemastore.org/chart'] = 'Chart.{yml,yaml}',
+        ['https://json.schemastore.org/dependabot-v2'] = '.github/dependabot.{yml,yaml}',
+        ['https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json'] = '*gitlab-ci*.{yml,yaml}',
+        ['https://raw.githubusercontent.com/OAI/OpenAPI-Specification/main/schemas/v3.1/schema.json'] = '*api*.{yml,yaml}',
+        ['https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json'] = '*docker-compose*.{yml,yaml}',
+        ['https://raw.githubusercontent.com/argoproj/argo-workflows/master/api/jsonschema/schema.json'] = '*flow*.{yml,yaml}',
+        ['https://raw.githubusercontent.com/yannh/kubernetes-json-schema/refs/heads/master/v1.32.1-standalone-strict/all.json'] = '/*.k8s.yaml',
+      },
+    },
+  },
+}
+vim.lsp.config('yamlls', yamlls)
 
 -- local stylua = {
 --   cmd = { 'stylua', '--search-parent-directories', '--stdin-filepath', '$FILENAME', '-' },
