4bcc536b8169a93dbaee6b90cb1136afaaaf0924
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-10 04:55:33 +0300

    update

--- lua/platformio/boilerplate.lua
+++ lua/platformio/boilerplate.lua
@@ -1,4 +1,3 @@
-local pio = require('platformio.utils.pio')
 local M = {}
 local uv = vim.loop
 
@@ -43,6 +42,7 @@ monitor_dtr = 0   ; 0 // pio dev mon --rts=0 --dtr=0 then pio dev mon --rts=1 dt
 lib_ldf_mode = chain+   ;Library dependencies Finder ldf
 ]],
   content = function(self)
+    local pio = require('platformio.utils.pio')
     return string.format(self.template, pio.get_pio_dir('core'))
   end,
 }
@@ -83,6 +83,7 @@ clangd
 --query-driver=%s/toolchain-*/**/bin/*
 ]],
   content = function(self)
+    local pio = require('platformio.utils.pio')
     return string.format(self.template, pio.get_pio_dir('packages') or '**')
   end,
   --query-driver=%s/.platformio/packages/*/bin/riscv32-esp-elf-*
