99a284d239b4eef4b2c340bd7478991d1d58198d
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-16 21:58:13 +0300

    update

--- lua/platformio/utils/lsp.lua
+++ lua/platformio/utils/lsp.lua
@@ -2,14 +2,14 @@ local M = {}
 
 --- stylua: ignore
 function M.lsp_restart(name)
-  vim.schedule_wrap(function()
-    local clangConfig = _G.get_clangd_config()
-    print(vim.inspect(clangConfig))
-    vim.lsp.config(name, clangConfig)
-    vim.lsp.enable(name, false)
-    vim.lsp.enable(name, true)
-    vim.cmd('checktime')
-  end)
+  -- vim.schedule_wrap(function()
+  local clangConfig = _G.get_clangd_config()
+  print(vim.inspect(clangConfig))
+  vim.lsp.config(name, clangConfig)
+  vim.lsp.enable(name, false)
+  vim.lsp.enable(name, true)
+  vim.cmd('checktime')
+  -- end)
 end
 
 return M
