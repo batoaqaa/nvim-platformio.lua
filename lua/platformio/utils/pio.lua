9bda76ff2e59733d7d88289563527aa16d0bb8d4
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-20 17:40:46 +0300

    update

--- lua/platformio/utils/pio.lua
+++ lua/platformio/utils/pio.lua
@@ -260,7 +260,9 @@ M.run_sequence = function(tasks)
   _G.metadata.isBusy = true
   -- local ToggleTerminal = require('platformio.utils.term').ToggleTerminal
   -- ToggleTerminal(full_cmd, 'float')
-  if M.term then M.term.ToggleTerminal(full_cmd, 'float') end
+  -- if M.term then 
+    M.term(full_cmd, 'float')
+  -- end
 
 end
 
