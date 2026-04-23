8d3a36085869576b183acdeeff10a408b554dd50
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-23 20:35:38 +0300

    update

--- lua/platformio/utils/pio.lua
+++ lua/platformio/utils/pio.lua
@@ -213,33 +213,44 @@ local callBack = nil
 local commandPassed = 0
 ------------------------------------------------------
 -- INFO: ToggleTerminal commands stdout filter
--- stylua: ignore
+--- stylua: ignore
 function M.stdoutcallback(_, _, data)
-  if #M.queue == 0 then return end
+  if #M.queue == 0 then
+    return
+  end
 
   -- 1. attach partial buffer from previous data last line to 1st line
   pio_buffer = pio_buffer .. data[1]
   -- 2. If the chunk has more than one element, we've encountered newlines
   if #data > 1 then
     -- 3. Process any "middle" lines which are guaranteed to be complete
-    for i = 2, #data - 1 do pio_buffer = pio_buffer .. data[i] end
+    for i = 2, #data - 1 do
+      pio_buffer = pio_buffer .. data[i]
+    end
 
     for status in pio_buffer:gmatch('_CMMNDS_:(%a+)') do
       if callBack and status then
         if status == 'PASS' then
           -- Store the last element as the new partial buffer for the next call
           pio_buffer = data[#data]
-          vim.schedule(function() callBack('PASS') end)
-        elseif status == 'DONE' then
-          vim.schedule(function() callBack('DONE') end)
-        elseif status == 'FAIL' then
-          vim.schedule(function() callBack('DONE') end)
         end
+        -- vim.schedule(function() callBack('PASS') end)
+        --   callBack('PASS')
+        -- elseif status == 'DONE' then
+        -- callBack('PASS')
+        -- vim.schedule(function() callBack('DONE') end)
+        -- elseif status == 'FAIL' then
+        -- callBack('PASS')
+        callBack(status)
+        -- vim.schedule(function() callBack('DONE') end)
+        -- end
         break
       end
     end
   end
-  if #pio_buffer > 10000 then pio_buffer = pio_buffer:sub(-5000) end
+  if #pio_buffer > 10000 then
+    pio_buffer = pio_buffer:sub(-5000)
+  end
 end
 
 -- stylua: ignore
@@ -275,15 +286,15 @@ function M.handlePioinit(result)
   elseif result == 'PASS' then
     commandPassed = commandPassed + 1
     if commandPassed == 1 then
-      -- vim.schedule(function()
-      vim.notify('Pioinit: commandPassed', vim.log.levels.INFO)
-      local pio_manager = require('platformio.pio_setup').pio_manager
-      pio_manager.refresh(function()
-        local boilerplate_gen = require('platformio.boilerplate').boilerplate_gen
-        boilerplate_gen(M.selected_framework, vim.uv.cwd() .. '/src', 'main.cpp')
-        boilerplate_gen([[.clangd]], _G.metadata.core_dir)
+      vim.schedule(function()
+        vim.notify('Pioinit: commandPassed', vim.log.levels.INFO)
+        local pio_manager = require('platformio.pio_setup').pio_manager
+        pio_manager.refresh(function()
+          local boilerplate_gen = require('platformio.boilerplate').boilerplate_gen
+          boilerplate_gen(M.selected_framework, vim.uv.cwd() .. '/src', 'main.cpp')
+          boilerplate_gen([[.clangd]], _G.metadata.core_dir)
+        end)
       end)
-      -- end)
       -- elseif commandPassed == 2 then
     end
     local full_cmd = table.remove(M.queue, 1)
@@ -292,11 +303,11 @@ function M.handlePioinit(result)
     pio_buffer = ''
     M.queue = {} -- Clear queue on any other status (failure)
     term.stdout_callback = nil
-    -- vim.schedule(function()
-    vim.notify('compiledb: Pass', vim.log.levels.INFO)
-    vim.misc.gitignore_lsp_configs('compile_commands.json')
-    _G.metadata.dbTrigger = true
-    -- end)
+    vim.schedule(function()
+      vim.notify('compiledb: Pass', vim.log.levels.INFO)
+      vim.misc.gitignore_lsp_configs('compile_commands.json')
+      _G.metadata.dbTrigger = true
+    end)
   elseif result == 'FAIL' then
     pio_buffer = ''
     M.queue = {} -- Clear queue on any other status (failure)
