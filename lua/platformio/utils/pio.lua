f27db6fd67139e1d76b812ead1702bbaa6d53c7a
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-25 19:42:42 +0300

    update

--- lua/platformio/utils/pio.lua
+++ lua/platformio/utils/pio.lua
@@ -129,7 +129,8 @@ M.run_sequence = function(tasks)
   M.queue = {}
   callBack = tasks.cb -- 1. Save the callback in a local variable
   local commands = tasks.cmnds
-
+  print(#commands)
+   
   local done = ' && echo _CMMNDS_":"DONE'
   local pass = ' && echo _CMMNDS_":"PASS'
   local fail = ' || echo _CMMNDS_":"FAIL'
