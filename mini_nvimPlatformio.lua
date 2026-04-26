c3e0b913b80f6a1d24a7ad38658115ab2c5f67b1
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-26 16:08:56 +0300

    update

--- mini_nvimPlatformio.lua
+++ mini_nvimPlatformio.lua
@@ -262,108 +262,114 @@ local plugins = {
   },
 
   {
+    -- 'nvim-neo-tree/neo-tree.nvim',
+    -- -- cmd = 'Neotree',
+    -- lazy = false,
+    -- dependencies = 'nvim-tree/nvim-web-devicons',
+    -- keys = {
+    --   {
+    --     '\\',
+    --     function()
+    --       require('neo-tree.command').execute({ toggle = true, dir = vim.uv.cwd() })
+    --     end,
+    --     desc = 'Explorer NeoTree (cwd)',
+    --   },
+    --   { '<leader>e', '<leader>fe', desc = 'Explorer NeoTree (Root Dir)', remap = true },
+    --   { '<leader>E', '<leader>fE', desc = 'Explorer NeoTree (cwd)', remap = true },
+    --   {
+    --     '<leader>ge',
+    --     function()
+    --       require('neo-tree.command').execute({ source = 'git_status', toggle = true })
+    --     end,
+    --     desc = 'Git Explorer',
+    --   },
+    --   {
+    --     '<leader>be',
+    --     function()
+    --       require('neo-tree.command').execute({ source = 'buffers', toggle = true })
+    --     end,
+    --     desc = 'Buffer Explorer',
+    --   },
+    -- },
+    -- opts = {
+    --   sources = { 'filesystem', 'buffers', 'git_status' },
+    --   open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
+    --   filesystem = {
+    --     bind_to_cwd = false,
+    --     follow_current_file = { enabled = true },
+    --     use_libuv_file_watcher = true,
+    --   },
+    --   window = {
+    --     mappings = {
+    --       ['l'] = 'open',
+    --       ['h'] = 'close_node',
+    --       ['<space>'] = 'none',
+    --       ['Y'] = {
+    --         function(state)
+    --           local node = state.tree:get_node()
+    --           local path = node:get_id()
+    --           vim.fn.setreg('+', path, 'c')
+    --         end,
+    --         desc = 'Copy Path to Clipboard',
+    --       },
+    --       ['O'] = {
+    --         function(state)
+    --           require('lazy.util').open(state.tree:get_node().path, { system = true })
+    --         end,
+    --         desc = 'Open with System Application',
+    --       },
+    --       ['P'] = { 'toggle_preview', config = { use_float = false } },
+    --     },
+    --   },
+    --   default_component_configs = {
+    --     indent = {
+    --       with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
+    --       expander_collapsed = '',
+    --       expander_expanded = '',
+    --       expander_highlight = 'NeoTreeExpander',
+    --     },
+    --     git_status = {
+    --       symbols = {
+    --         unstaged = '󰄱',
+    --         staged = '󰱒',
+    --       },
+    --     },
+    --   },
+    -- },
     'nvim-neo-tree/neo-tree.nvim',
-    -- cmd = 'Neotree',
-    lazy = false,
-    dependencies = 'nvim-tree/nvim-web-devicons',
-    keys = {
-      {
-        '\\',
-        function()
-          require('neo-tree.command').execute({ toggle = true, dir = vim.uv.cwd() })
-        end,
-        desc = 'Explorer NeoTree (cwd)',
-      },
-      { '<leader>e', '<leader>fe', desc = 'Explorer NeoTree (Root Dir)', remap = true },
-      { '<leader>E', '<leader>fE', desc = 'Explorer NeoTree (cwd)', remap = true },
-      {
-        '<leader>ge',
-        function()
-          require('neo-tree.command').execute({ source = 'git_status', toggle = true })
-        end,
-        desc = 'Git Explorer',
-      },
-      {
-        '<leader>be',
-        function()
-          require('neo-tree.command').execute({ source = 'buffers', toggle = true })
-        end,
-        desc = 'Buffer Explorer',
-      },
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      'nvim-tree/nvim-web-devicons', -- optional, but recommended
     },
+    lazy = false, -- neo-tree will lazily load itself
     opts = {
-      sources = { 'filesystem', 'buffers', 'git_status' },
+      -- use_libuv_file_watcher = true,
       open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
       filesystem = {
         bind_to_cwd = false,
         follow_current_file = { enabled = true },
         use_libuv_file_watcher = true,
       },
-      window = {
-        mappings = {
-          ['l'] = 'open',
-          ['h'] = 'close_node',
-          ['<space>'] = 'none',
-          ['Y'] = {
-            function(state)
-              local node = state.tree:get_node()
-              local path = node:get_id()
-              vim.fn.setreg('+', path, 'c')
-            end,
-            desc = 'Copy Path to Clipboard',
-          },
-          ['O'] = {
-            function(state)
-              require('lazy.util').open(state.tree:get_node().path, { system = true })
-            end,
-            desc = 'Open with System Application',
-          },
-          ['P'] = { 'toggle_preview', config = { use_float = false } },
-        },
+      sources = {
+        'filesystem',
+        'buffers',
+        'git_status',
+        'document_symbols', -- Add this line
       },
-      default_component_configs = {
-        indent = {
-          with_expanders = true, -- if nil and file nesting is enabled, will enable expanders
-          expander_collapsed = '',
-          expander_expanded = '',
-          expander_highlight = 'NeoTreeExpander',
-        },
-        git_status = {
-          symbols = {
-            unstaged = '󰄱',
-            staged = '󰱒',
-          },
-        },
-      },
+      -- filtered_items = {
+      --   visible = false, -- Change to true if you want to see them dimmed
+      --   hide_dotfiles = true,
+      --   hide_gitignored = true,
+      --   hide_by_name = { '.cache', '.pio' },
+      --   never_show = { -- This is the strongest setting to ignore the folder
+      --     '.cache',
+      --     '.git',
+      --     'node_modules',
+      --   },
+      -- },
     },
-    -- 'nvim-neo-tree/neo-tree.nvim',
-    -- branch = 'v3.x',
-    -- dependencies = {
-    --   'nvim-lua/plenary.nvim',
-    --   'MunifTanjim/nui.nvim',
-    --   'nvim-tree/nvim-web-devicons', -- optional, but recommended
-    -- },
-    -- lazy = false, -- neo-tree will lazily load itself
-    -- opts = {
-    --   use_libuv_file_watcher = true,
-    --   sources = {
-    --     'filesystem',
-    --     'buffers',
-    --     'git_status',
-    --     'document_symbols', -- Add this line
-    --   },
-    --   -- filtered_items = {
-    --   --   visible = false, -- Change to true if you want to see them dimmed
-    --   --   hide_dotfiles = true,
-    --   --   hide_gitignored = true,
-    --   --   hide_by_name = { '.cache', '.pio' },
-    --   --   never_show = { -- This is the strongest setting to ignore the folder
-    --   --     '.cache',
-    --   --     '.git',
-    --   --     'node_modules',
-    --   --   },
-    --   -- },
-    -- },
   },
 
   -- {
