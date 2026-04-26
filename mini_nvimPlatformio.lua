bf9ad09b308204ecc60446c650091a68b25bdfc2
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-26 17:24:47 +0300

    update

--- mini_nvimPlatformio.lua
+++ mini_nvimPlatformio.lua
@@ -369,11 +369,87 @@ local plugins = {
 
 ----------------------------------------------------------------------------------------
 -- INFO: Install/config plugins
+-- require('lazy').setup(plugins, {
+--   install = {
+--     missing = true,
+--   },
+-- })
+
 require('lazy').setup(plugins, {
-  install = {
-    missing = true,
+  rocks = { enabled = false },
+  git = { log = { '--since=3 days ago' } },
+  ui = {
+    custom_keys = { false },
+    defaults = {
+      lazy = true,
+    },
+    -- a number <1 is a percentage., >1 is a fixed size
+    size = { width = 0.8, height = 0.8 },
+    wrap = true, -- wrap the lines in the ui
+    -- The border to use for the UI window. Accepts same border values as |nvim_open_win()|.
+    border = 'none',
+    -- The backdrop opacity. 0 is fully opaque, 100 is fully transparent.
+    backdrop = 60,
+    title = nil, ---@type string only works when border is not "none"
+    title_pos = 'center', ---@type "center" | "left" | "right"
+    -- Show pills on top of the Lazy window
+    pills = true, ---@type boolean
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = ' ',
+      config = '🛠',
+      debug = '● ',
+      event = '📅',
+      favorite = ' ',
+      ft = ' ',
+      init = '⚙',
+      import = ' ',
+      keys = ' ',
+      lazy = '󰒲 ',
+      loaded = '●',
+      not_loaded = '○',
+      plugin = ' ',
+      runtime = '💻',
+      require = '󰢱 ',
+      source = '📄',
+      start = ' ',
+      task = '✔ ',
+      list = {
+        '●',
+        '➜',
+        '★',
+        '‒',
+      },
+    },
+  },
+  -- install = { colorscheme = { 'catppuccin' } },
+  performance = {
+    rtp = {
+      disabled_plugins = {
+        'gzip',
+        'netrwPlugin',
+        'tarPlugin',
+        'tohtml',
+        'tutor',
+        'zipPlugin',
+        'rplugin',
+        'editorconfig',
+        'matchparen',
+        'matchit',
+      },
+    },
+  },
+  checker = { enabled = false },
+  change_detection = {
+    enabled = true,
+    notify = false,
   },
 })
+
+
+
+
+
+
 ----------------------------------------------------------------------------------------
 
 -- stylua: ignore
