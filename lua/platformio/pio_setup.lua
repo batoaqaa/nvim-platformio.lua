a37a43b4b2f9edd4ce4ec6ba877b1193ed1162af
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-26 13:03:42 +0300

    update

--- lua/platformio/pio_setup.lua
+++ lua/platformio/pio_setup.lua
@@ -492,7 +492,7 @@ function M.init()
       -- boilerplate_gen([[.clangd]], vim.fs.joinpath(vim.env.XDG_CONFIG_HOME, 'clangd'), 'config.yaml')
       -- boilerplate_gen([[.clangd]], _G.metadata.core_dir)
       boilerplate.core_dir = _G.metadata.core_dir
-      boilerplate_gen([[platformio.ini]], vim.g.platformioRootDir)
+      -- boilerplate_gen([[platformio.ini]], vim.g.platformioRootDir)
       boilerplate_gen([[.clang-format]], vim.g.platformioRootDir)
       ---------------------------------------------------------------------------------
       -- M.run_compiledb() -- Smart: Auto-update DB if config changes
