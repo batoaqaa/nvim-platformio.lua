fe00eb989922ca125aed04846fca0b4fb35250e5
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-28 12:25:18 +0300

    update

--- lua/platformio/pio_setup.lua
+++ lua/platformio/pio_setup.lua
@@ -367,7 +367,6 @@ function M.start_watchers()
       idedata_path = vim.misc.joinPath(project_root, '.pio/build', active_env, 'idedata.json'),
       path = vim.misc.joinPath(project_root, '.pio/build', 'project.checksum'), --checksum_path
       cb = function(self)
-        print(self.path)
         local _, current_checksum = vim.misc.readFile(self.path)
         if current_checksum and current_checksum ~= '' then
           if current_checksum == _G.metadata.last_checksum then
@@ -376,7 +375,7 @@ function M.start_watchers()
 
           -- STEP 2: Cache Path (idedata.json exists and checksum changed)
           M.pio_refresh(function()
-            local dbFix = require('platformio.utils.pio').compile_commandsFix
+            -- local dbFix = require('platformio.utils.pio').compile_commandsFix
             -- dbFix()
             vim.notify('DB Updated', vim.log.levels.INFO, { title = 'PlatformIO' })
           end)
@@ -385,6 +384,10 @@ function M.start_watchers()
     },
   }
   targets[1].current_ini_hash = get_hash(targets[1].path) or ''
+  -- targets[1].current_ini_hash = get_hash(targets[1].path) or ''
+  -- targets[1].path = vim.misc.joinPath(project_root, 'platformio.ini')
+  -- targets[2].path = vim.misc.joinPath(project_root, '.pio/build', 'project.checksum') --checksum_path
+  -- targets[2].idedata_path = vim.misc.joinPath(project_root, '.pio/build', active_env, 'idedata.json')
 
   for _, target in ipairs(targets) do
     local h = watch_file(target.path, target.cb)
