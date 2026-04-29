607be9580185ef256a0ed88b30e6bd0d5187a39c
batoaqaa <ahmed.batoaq@gmail.com>
2026-04-29 06:22:19 +0300

    update

--- lua/platformio/pio_setup.lua
+++ lua/platformio/pio_setup.lua
@@ -661,8 +661,7 @@ function M.init()
     end
 
     -- Always start the watcher so it can catch a future 'pio init'
-    -- M.start_pio_watcher()
-    -- M.start_watchers()
+    M.start_watchers()
 
     -- boilerplate_gen([[platformio.ini]], vim.g.platformioRootDir)
     -- If the file already exists, do an initial sync
