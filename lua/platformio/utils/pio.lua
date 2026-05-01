6ba58b9d8b2801484ac6f18135ca1c40820ca2dd
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-01 10:38:21 +0300

    update

--- lua/platformio/utils/pio.lua
+++ lua/platformio/utils/pio.lua
@@ -660,7 +660,7 @@ function M.handlePioinit(result)
       pio_refresh(function()
         lsp_restart('clangd')
         vim.misc.closeMessage(win_id)
-        term.toggle_all()
+        term.close()
         -- term.ToggleTerminal('echo "************ project Initialization success ************"', 'float')
       end, 'PIO init: ')
     end)
@@ -669,7 +669,7 @@ function M.handlePioinit(result)
     _G.metadata.isBusy = false
   elseif result == 'FAIL' then
     vim.misc.closeMessage(win_id)
-    term.toggle_all()
+    term.close()
     M.queue = {}
     term.stdout_callback = nil
     _G.metadata.isBusy = false
