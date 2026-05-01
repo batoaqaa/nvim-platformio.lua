b10209a2b0d463f706bffc46e04508a8d376ee44
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-01 23:52:26 +0300

    update

--- lua/platformio/pio_setup.lua
+++ lua/platformio/pio_setup.lua
@@ -76,12 +76,12 @@ function M.run_compiledb(target)
         if obj.code == 0 then
           -- vim.notify('DB Updated Successfully', vim.log.levels.INFO, { title = 'PlatformIO' })
           -- Trigger refresh (LSP restart, etc.)
-          -- vim.schedule(function ()
-          -- M.pio_refresh(function()
-          clangdRestart()
-          vim.notify('PIO platformio.ini change: Update Success', vim.log.levels.INFO, { title = 'PlatformIO' })
-          -- end, 'PIO platformio.ini  change: ')
-          -- end)
+          vim.schedule(function ()
+            M.pio_refresh(function()
+              clangdRestart()
+              vim.notify('PIO platformio.ini change: Update Success', vim.log.levels.INFO, { title = 'PlatformIO' })
+            end, 'PIO platformio.ini  change: ')
+          end)
         else
           local err = (obj.stderr and obj.stderr ~= '') and obj.stderr or 'Check PIO logs'
           vim.notify('PIO Build Failed: ' .. err, vim.log.levels.ERROR, { title = 'PlatformIO' })
