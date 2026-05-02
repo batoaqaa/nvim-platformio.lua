abebdaf4d40ac56ca22763bd9e0deb4b3e8c1e7f
batoaqaa <ahmed.batoaq@gmail.com>
2026-05-02 22:55:44 +0300

    update

--- lua/platformio/lspConfig/clangd.lua
+++ lua/platformio/lspConfig/clangd.lua
@@ -112,9 +112,18 @@ function _G.get_clangd_config()
   if _G.metadata and _G.metadata.cc_compiler and  _G.metadata.cc_compiler ~= '' then
     if _G.metadata.triplet and _G.metadata.triplet ~= '' then
       -- local include_flags = table.concat(_G.metadata.fallbackFlags, ", ")
-      local includes_toolchain = table.concat(_G.metadata.includes_toolchain, ", ")
+      -- local includes_toolchain = table.concat(_G.metadata.includes_toolchain, ", ")
+
+      local include_flags = table.concat(vim.tbl_map(function(item)
+        return '"' .. item .. '"'
+      end, _G.metadata.fallbackFlags), ", ")
+
+      local includes_toolchain = table.concat(vim.tbl_map(function(item)
+        return '"' .. item .. '"'
+      end, _G.metadata.includes_toolchain), ", ")
+
       -- f_flags = '"-std=c++17", "-xc++"'
-      f_flags = string.format([["-std=c++17", "-xc++", "-D__cplusplus=201703L", "--target=%s", "--sysroot=%s", %s]], _G.metadata.triplet, _G.metadata.sysroot, includes_toolchain)
+      f_flags = string.format([["-std=c++17", "-xc++", "-D__cplusplus=201703L", "--target=%s", "--sysroot=%s", %s, %s]], _G.metadata.triplet, _G.metadata.sysroot, includes_toolchain, include_flags)
       -- f_flags = string.format('"--sysroot=%s"', _G.metadata.sysroot)
       -- f_flags = string.format([["--sysroot=%s", %s]], _G.metadata.sysroot, include_flags)
 
